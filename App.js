b2b3184d13e9073b47c4a5996148a691d9fba082
Chau Tran <cctran96@gmail.com>
2021-09-29 17:28:42 -0700

    add some images

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+        <ImageBackground 
+            style={styles.background} 
+            resizeMode="cover" 
+            source={require("./assets/main/background.png")}
+        >
+            <Text>Sup</Text>
+            <StatusBar style="auto" />
+        </ImageBackground>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+    },
+    background: {
+        flex: 1,
+        justifyContent: "center"
+    }
 });
